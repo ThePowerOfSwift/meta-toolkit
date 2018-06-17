7984cc42bc87b2a2d99c0adb4072931e74a1bf92
Ben Guhin <benguhin@gmail.com>
2018-06-17 00:57:25 -0500

    test p

--- templates/listing_detail2.php
+++ templates/listing_detail2.php
@@ -204,6 +204,7 @@ if ( have_posts() ) {
 			<div class="post-meta-info">
 				<div class="container">
 					<div class="top-row-detail">
+						<p>test something here</p>
 						<div class="col-md-8 col-sm-8 col-xs-12">
 							<?php if (function_exists('listingpro_breadcrumbs')) listingpro_breadcrumbs(); ?>
 						</div>
