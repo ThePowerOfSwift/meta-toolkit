63f5a91b4520b785f3673c59f978f92534d9c3f3
Ben Guhin <benguhin@gmail.com>
2018-06-17 11:16:26 -0500

    new language for claim section

--- templates/single-list/claimed.php
+++ templates/single-list/claimed.php
@@ -32,7 +32,7 @@ if($showClaim==true){
 			<span class="phone-icon">
 				<!-- <i class="fa fa-building-o"></i> -->
 				<?php echo listingpro_icons('building'); ?>
-				<strong><?php echo esc_html__('Own or work here?', 'listingpro'); ?></strong>
+				<strong><?php echo esc_html__('Own this resource?', 'listingpro'); ?></strong>
 			</span>
 		<?php
 			/* added by zaheer on 25 feb */
@@ -43,28 +43,28 @@ if($showClaim==true){
 
 				?>
 					<a class="phone-number md-trigger claimformtrigger2" data-modal="modal-2">
-						<?php echo esc_html__('Claim Now!', 'listingpro'); ?>
+						<?php echo esc_html__('Add your info!', 'listingpro'); ?>
 					</a>
 					<a class="phone-number claimformtrigger md-trigger">
-						<?php echo esc_html__('Claim Now!', 'listingpro'); ?>
+						<?php echo esc_html__('Add your info!', 'listingpro'); ?>
 					</a>
 					<?php
 				} else { ?>
 					<?php if( $listing_mobile_view == 'app_view' && wp_is_mobile() ){?>
 
 						<a class="phone-number md-trigger claimformtrigger2" data-toggle="modal" data-target="#app-view-login-popup">
-							<?php echo esc_html__('Claim Now!', 'listingpro'); ?>
+							<?php echo esc_html__('Add your info!', 'listingpro'); ?>
 						</a>
 						<a class="phone-number claimformtrigger md-trigger" data-toggle="modal" data-target="#app-view-login-popup">
-							<?php echo esc_html__('Claim Now!', 'listingpro'); ?>
+							<?php echo esc_html__('Add your info!', 'listingpro'); ?>
 						</a>
 					<?php } else{ ?>
 
 						<a class="phone-number md-trigger claimformtrigger2" data-modal="modal-3">
-							<?php echo esc_html__('Claim Now!', 'listingpro'); ?>
+							<?php echo esc_html__('Add your info!', 'listingpro'); ?>
 						</a>
 						<a class="phone-number claimformtrigger md-trigger" data-modal="modal-3">
-							<?php echo esc_html__('Claim Now!', 'listingpro'); ?>
+							<?php echo esc_html__('Add your info!', 'listingpro'); ?>
 						</a>
 
 
